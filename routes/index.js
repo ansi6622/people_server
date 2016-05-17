cc8bc2aebdae55c58b9609e29420e2ca874a2329
Anthony Simpson <ansi6622@colorado.edu>
2016-05-17 11:27:01 -0600

    congigged the pkg.json properly

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
+require('dotenv').load()
 var express = require('express');
 var router = express.Router();
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.json({ title: 'Express' });
