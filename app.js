9f75fe50f11f5d5cbfe82a99f0c1556412f9b522
Anthony Simpson <ansi6622@colorado.edu>
2016-05-17 11:31:42 -0600

    adding boilerplate knex stuff

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var knex = require('./db/knex.js')
 var routes = require('./routes/index');
 
 var app = express();
